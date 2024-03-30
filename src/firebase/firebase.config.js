2e1c21a1f32e9de4937562144990488a75f793c9
Zioun <jionkhan0@gmail.com>
2024-03-30 10:08:01 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqwUTro5pFlDK3IlDBTUGat0dXQLNKBTI",
+  authDomain: "first-fortress-377613.firebaseapp.com",
+  projectId: "first-fortress-377613",
+  storageBucket: "first-fortress-377613.appspot.com",
+  messagingSenderId: "354314161947",
+  appId: "1:354314161947:web:f814c268fc415c917a8dbe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
